2e977640218edb08e2b29af068c2a6ec01a92840
ph1lb4 <philipp.baldauf@gmail.com>
2014-06-20 11:08:59 +0200

    changed incorrect naming of listener interface

--- MPChartLib/src/com/github/mikephil/charting/interfaces/OnDrawLineChartTouchListener.java
+++ MPChartLib/src/com/github/mikephil/charting/interfaces/OnDrawLineChartTouchListener.java
@@ -1,11 +1,11 @@
-package com.github.mikephil.charting.listener;
+package com.github.mikephil.charting.interfaces;
 
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 
-public class DrawLineChartTouchListener extends SimpleOnGestureListener implements OnTouchListener {
+public class OnDrawLineChartTouchListener extends SimpleOnGestureListener implements OnTouchListener {
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
