473134d39a36809bc4c980dbdcf675ec203ff6e0
Robert Schmid <robertschmid@airstriptech.com>
2014-11-20 14:37:39 -0600

    Convert chart from View to ViewGroup

--- MPChartLib/src/com/github/mikephil/charting/charts/Chart.java
+++ MPChartLib/src/com/github/mikephil/charting/charts/Chart.java
@@ -22,6 +22,7 @@ import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewParent;
 
 import com.github.mikephil.charting.data.BarData;
@@ -54,7 +55,8 @@ import java.util.ArrayList;
  * 
  * @author Philipp Jahoda
  */
-public abstract class Chart<T extends ChartData<? extends DataSet<? extends Entry>>> extends View
+public abstract class Chart<T extends ChartData<? extends DataSet<? extends Entry>>>
+		extends ViewGroup
         implements ValueAnimator.AnimatorUpdateListener, ChartInterface {
 
     public static final String LOG_TAG = "MPChart";
@@ -2176,7 +2178,6 @@ public abstract class Chart<T extends ChartData<? extends DataSet<? extends Entr
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
 
         prepareContentRect();
 
