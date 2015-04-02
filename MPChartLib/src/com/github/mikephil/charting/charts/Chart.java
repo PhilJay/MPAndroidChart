dabcaa7ee6567c42c86801e979f218b2c9088886
Philipp Jahoda <phil.jahoda@gmail.com>
2015-04-02 14:18:46 +0200

    Fixed no data bug.

--- MPChartLib/src/com/github/mikephil/charting/charts/Chart.java
+++ MPChartLib/src/com/github/mikephil/charting/charts/Chart.java
@@ -247,7 +247,7 @@ public abstract class Chart<T extends ChartData<? extends DataSet<? extends Entr
      */
     public void setData(T data) {
 
-        if (data == null || data.getYValCount() <= 0) {
+        if (data == null) {
             Log.e(LOG_TAG,
                     "Cannot set data for chart. Provided data object is null.");
             return;
@@ -365,7 +365,7 @@ public abstract class Chart<T extends ChartData<? extends DataSet<? extends Entr
     protected void onDraw(Canvas canvas) {
         // super.onDraw(canvas);
 
-        if (mDataNotSet) { // check if there is data
+        if (mDataNotSet || mData == null || mData.getYValCount() <= 0) { // check if there is data
 
             // if no data, inform the user
             canvas.drawText(mNoDataText, getWidth() / 2, getHeight() / 2, mInfoPaint);
