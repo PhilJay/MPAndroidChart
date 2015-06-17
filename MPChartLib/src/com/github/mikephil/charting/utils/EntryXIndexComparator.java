8cf93369350bc55b54620f872cdbb1d1fd3978d8
Philipp Jahoda <phil.jahoda@gmail.com>
2015-06-17 17:33:26 +0200

    Added comparator for comparing entries by their x-index.

---
+++ MPChartLib/src/com/github/mikephil/charting/utils/EntryXIndexComparator.java
@@ -0,0 +1,16 @@
+package com.github.mikephil.charting.utils;
+
+import com.github.mikephil.charting.data.Entry;
+
+import java.util.Comparator;
+
+/**
+ * Comparator for comparing Entry-objects by their x-index.
+ * Created by philipp on 17/06/15.
+ */
+public class EntryXIndexComparator implements Comparator<Entry> {
+    @Override
+    public int compare(Entry entry1, Entry entry2) {
+        return entry1.getXIndex() - entry2.getXIndex();
+    }
+}
