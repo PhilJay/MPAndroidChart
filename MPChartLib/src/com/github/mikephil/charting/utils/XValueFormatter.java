2e6bfeb87e2bb0a02fb6350b96a454d77f7c088b
Philipp Jahoda <phil.jahoda@gmail.com>
2015-09-14 21:01:15 +0200

    Add documentation.

--- MPChartLib/src/com/github/mikephil/charting/utils/XValueFormatter.java
+++ MPChartLib/src/com/github/mikephil/charting/utils/XValueFormatter.java
@@ -8,5 +8,13 @@ package com.github.mikephil.charting.utils;
  */
 public interface XValueFormatter {
 
+    /**
+     * Returns the customized label that is drawn on the x-axis.
+     *
+     * @param original        the original x-axis label to be drawn
+     * @param index           the x-index that is currently being drawn
+     * @param viewPortHandler provides information about the current chart state (scale, translation, ...)
+     * @return
+     */
     String getXValue(String original, int index, ViewPortHandler viewPortHandler);
 }
