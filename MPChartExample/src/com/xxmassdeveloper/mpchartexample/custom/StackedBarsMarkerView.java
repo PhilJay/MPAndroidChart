5f5950b4414fc6294f333870deda0341ea0dd9fc
Philipp Jahoda <phil.jahoda@gmail.com>
2015-10-23 13:58:43 +0200

    Fix issue #1136

--- MPChartExample/src/com/xxmassdeveloper/mpchartexample/custom/StackedBarsMarkerView.java
+++ MPChartExample/src/com/xxmassdeveloper/mpchartexample/custom/StackedBarsMarkerView.java
@@ -50,13 +50,13 @@ public class StackedBarsMarkerView extends MarkerView {
     }
 
     @Override
-    public int getXOffset() {
+    public int getXOffset(float xpos) {
         // this will center the marker-view horizontally
         return -(getWidth() / 2);
     }
 
     @Override
-    public int getYOffset() {
+    public int getYOffset(float ypos) {
         // this will cause the marker-view to be above the selected value
         return -getHeight();
     }
