72bd6d904e9bc90411ff16b05cebf66f42a2356d
Philipp Jahoda <phil.jahoda@gmail.com>
2015-10-28 00:14:42 +0100

    Delete data interfaces

--- MPChartLib/src/com/github/mikephil/charting/interfaces/datainterfaces/data/ILineData.java
+++
@@ -1,9 +0,0 @@
-package com.github.mikephil.charting.interfaces.datainterfaces.data;
-
-import com.github.mikephil.charting.interfaces.datainterfaces.datasets.ILineDataSet;
-
-/**
- * Created by Philipp Jahoda on 21/10/15.
- */
-public interface ILineData<ILineDataSet> extends IBarLineScatterCandleBubbleData {
-}
