a4d1946486c95f67534fdcbd48153a49235f75f6
Philipp Jahoda <phil.jahoda@gmail.com>
2015-11-03 21:45:04 +0100

    Add IRadarDataSet to RadarData object

--- MPChartLib/src/com/github/mikephil/charting/data/RadarData.java
+++ MPChartLib/src/com/github/mikephil/charting/data/RadarData.java
@@ -1,46 +1,48 @@
 
 package com.github.mikephil.charting.data;
 
+import com.github.mikephil.charting.interfaces.datainterfaces.datasets.IRadarDataSet;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Data container for the RadarChart.
- * 
+ *
  * @author Philipp Jahoda
  */
-public class RadarData extends ChartData<RadarDataSet> {
+public class RadarData extends ChartData<IRadarDataSet> {
 
     public RadarData() {
         super();
     }
-    
+
     public RadarData(List<String> xVals) {
         super(xVals);
     }
-    
+
     public RadarData(String[] xVals) {
         super(xVals);
     }
-    
-    public RadarData(List<String> xVals, List<RadarDataSet> dataSets) {
+
+    public RadarData(List<String> xVals, List<IRadarDataSet> dataSets) {
         super(xVals, dataSets);
     }
 
-    public RadarData(String[] xVals, List<RadarDataSet> dataSets) {
+    public RadarData(String[] xVals, List<IRadarDataSet> dataSets) {
         super(xVals, dataSets);
     }
 
-    public RadarData(List<String> xVals, RadarDataSet dataSet) {
+    public RadarData(List<String> xVals, IRadarDataSet dataSet) {
         super(xVals, toList(dataSet));
     }
 
-    public RadarData(String[] xVals, RadarDataSet dataSet) {
+    public RadarData(String[] xVals, IRadarDataSet dataSet) {
         super(xVals, toList(dataSet));
     }
-    
-    private static List<RadarDataSet> toList(RadarDataSet dataSet) {
-        List<RadarDataSet> sets = new ArrayList<RadarDataSet>();
+
+    private static List<IRadarDataSet> toList(IRadarDataSet dataSet) {
+        List<IRadarDataSet> sets = new ArrayList<IRadarDataSet>();
         sets.add(dataSet);
         return sets;
     }
