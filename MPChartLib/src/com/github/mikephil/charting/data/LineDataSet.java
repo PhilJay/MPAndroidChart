dff935006b3c3d429b87dcb85d20b107da799c26
Ifeoma Okereke <okereke.ruth@gmail.com>
2016-03-17 14:27:24 -0700

    Fix inner radius bug.

--- MPChartLib/src/com/github/mikephil/charting/data/LineDataSet.java
+++ MPChartLib/src/com/github/mikephil/charting/data/LineDataSet.java
@@ -30,7 +30,7 @@ public class LineDataSet extends LineRadarDataSet<Entry> implements ILineDataSet
     private float mCircleRadius = 8f;
 
     /** the radius of the circular hole in the circle-shaped value indicators */
-    private float mInnerCircleRadius = mCircleRadius / 2;
+    private float mInnerCircleRadius = Utils.convertDpToPixel(mCircleRadius) / 2;
 
     /** sets the intensity of the cubic lines */
     private float mCubicIntensity = 0.2f;
@@ -74,6 +74,7 @@ public class LineDataSet extends LineRadarDataSet<Entry> implements ILineDataSet
         copied.mMode = mMode;
         copied.mColors = mColors;
         copied.mCircleRadius = mCircleRadius;
+        copied.mInnerCircleRadius = mInnerCircleRadius;
         copied.mCircleColors = mCircleColors;
         copied.mDashPathEffect = mDashPathEffect;
         copied.mDrawCircles = mDrawCircles;
