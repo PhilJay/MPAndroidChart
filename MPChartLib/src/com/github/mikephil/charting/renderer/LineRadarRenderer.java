0d1894770ce889a20a3d90108b5a72818a5d0bca
Philipp Jahoda <phil.jahoda@gmail.com>
2016-03-25 22:50:03 +0100

    Fix issue #1601

--- MPChartLib/src/com/github/mikephil/charting/renderer/LineRadarRenderer.java
+++ MPChartLib/src/com/github/mikephil/charting/renderer/LineRadarRenderer.java
@@ -1,7 +1,10 @@
 package com.github.mikephil.charting.renderer;
 
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.drawable.Drawable;
 
 import com.github.mikephil.charting.animation.ChartAnimator;
@@ -24,16 +27,22 @@ public abstract class LineRadarRenderer extends LineScatterCandleRadarRenderer {
      * @param drawable
      */
     protected void drawFilledPath(Canvas c, Path filledPath, Drawable drawable) {
-        c.save();
-        c.clipPath(filledPath);
 
-        drawable.setBounds((int) mViewPortHandler.contentLeft(),
-                (int) mViewPortHandler.contentTop(),
-                (int) mViewPortHandler.contentRight(),
-                (int) mViewPortHandler.contentBottom());
-        drawable.draw(c);
+        if (clipPathSupported()) {
 
-        c.restore();
+            c.save();
+            c.clipPath(filledPath);
+
+            drawable.setBounds((int) mViewPortHandler.contentLeft(),
+                    (int) mViewPortHandler.contentTop(),
+                    (int) mViewPortHandler.contentRight(),
+                    (int) mViewPortHandler.contentBottom());
+            drawable.draw(c);
+
+            c.restore();
+        } else {
+            throw new RuntimeException("Fill-drawables not (yet) supported below API level 18");
+        }
     }
 
     /**
@@ -46,11 +55,35 @@ public abstract class LineRadarRenderer extends LineScatterCandleRadarRenderer {
      * @param fillAlpha
      */
     protected void drawFilledPath(Canvas c, Path filledPath, int fillColor, int fillAlpha) {
-        c.save();
-        c.clipPath(filledPath);
 
         int color = (fillAlpha << 24) | (fillColor & 0xffffff);
-        c.drawColor(color);
-        c.restore();
+
+        if (clipPathSupported()) {
+
+            c.save();
+            c.clipPath(filledPath);
+
+            c.drawColor(color);
+            c.restore();
+        } else {
+
+            // save
+            Paint.Style previous = mRenderPaint.getStyle();
+            int previousColor = mRenderPaint.getColor();
+
+            // set
+            mRenderPaint.setStyle(Paint.Style.FILL);
+            mRenderPaint.setColor(color);
+
+            c.drawPath(filledPath, mRenderPaint);
+
+            // restore
+            mRenderPaint.setColor(previousColor);
+            mRenderPaint.setStyle(previous);
+        }
+    }
+
+    private boolean clipPathSupported() {
+        return android.os.Build.VERSION.SDK_INT >= 18;
     }
 }
