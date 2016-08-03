2aec119e8a8585d8cfd635b1d23534474fc04d41
Lars Schuetze <lars.schuetze@tu-dresden.de>
2016-08-03 10:28:11 +0200

    This is a suggestion to fix issue #2083 and maybe also #2070.
    The two ArrayList storing a value and its mapping to a formatted string represent a map. Thus, replacing the two ArrayList with a HashMap makes sense.
    Furthermore, the runtime behavior of this classes will change from O(n) to O(1) given that a lookup in the hash map is naturally faster than iterating over a list.
    The original implementation despite its announcement did not circumvent auto-boxing of the primitive values. The new version reduces the auto-boxing to one.

--- MPChartLib/src/main/java/com/github/mikephil/charting/formatter/FormattedStringCache.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/formatter/FormattedStringCache.java
@@ -6,59 +6,60 @@ import java.util.HashMap;
 
 /**
  * Created by Tony Patino on 6/29/16.
+ * <p>
+ * COST : Frequently the V type, and the K type will often be passed as primitives (float / int / double)
+ * This will incur an auto-boxing penalty, and an instantiation on each call.
+ * <p>
+ * BENEFIT : Formatting of Strings is one of the costliest operations remaining, and it is larger than the boxing penalty.
+ * Eliminating redundant formats helps more than boxing hurts.
+ * <p>
+ * Possibly want to make some explicit primitive enabled caches, though they can be ugly.
  *
- *      COST : Frequently the V type, and the K type will often be passed as primitives (float / int / double)
- *             This will incur an auto-boxing penalty, and an instantiation on each call.
- *
- *      BENEFIT : Formatting of Strings is one of the costliest operations remaining, and it is larger than the boxing penalty.
- *                Eliminating redundant formats helps more than boxing hurts.
- *
- *      Possibly want to make some explicit primitive enabled caches, though they can be ugly.
- *
+ * @author Lars Schütze <dev@larsschuteze.de>
+ *         Changed implementation slightly to circumvent issue #2083
  */
 public class FormattedStringCache {
 
     protected Format mFormat;
 
-    public Format getFormat(){
-        return mFormat;
-    }
-
-    public FormattedStringCache(Format format){
+    public FormattedStringCache(Format format) {
         this.mFormat = format;
     }
 
+    public Format getFormat() {
+        return mFormat;
+    }
 
     /**
      * Cache a Formatted String, derived from formatting value V in the Format passed to the constructor.
      * Use the object K as a way to quickly look up the string in the cache.
      * If value of V has changed since the last time the cache was accessed, re-format the string.
-     *
+     * <p>
      * NOTE: This version relies on auto-boxing of primitive values.  As a result, it has worse memory performance
      * than the Prim versions.
      *
      * @param <K>
      * @param <V>
      */
-    public static class Generic<K,V> extends FormattedStringCache{
+    public static class Generic<K, V> extends FormattedStringCache {
 
         private HashMap<K, String> mCachedStringsHashMap = new HashMap<>();
         private HashMap<K, V> mCachedValuesHashMap = new HashMap<>();
 
-        public Generic(Format format){
+        public Generic(Format format) {
             super(format);
         }
 
-        public String getFormattedValue(V value, K key){
+        public String getFormattedValue(V value, K key) {
 
             // If we can't find the value at all, create an entry for it, and format the string.
-            if(!mCachedValuesHashMap.containsKey(key)){
+            if (!mCachedValuesHashMap.containsKey(key)) {
                 mCachedStringsHashMap.put(key, mFormat.format(value));
                 mCachedValuesHashMap.put(key, value);
             }
 
             // If the old value and the new one don't match, format the string and store it, because the string's value will be different now.
-            if(!value.equals(mCachedValuesHashMap.get(key))){
+            if (!value.equals(mCachedValuesHashMap.get(key))) {
                 mCachedStringsHashMap.put(key, mFormat.format(value));
                 mCachedValuesHashMap.put(key, value);
             }
@@ -73,27 +74,25 @@ public class FormattedStringCache {
      * Cache a Formatted String, derived from formatting float value in the Format passed to the constructor.
      * Use the integer as a way to quickly look up the string in the cache.
      * If value of V has changed since the last time the cache was accessed, re-format the string.
-     *
      */
-    public static class PrimIntFloat extends FormattedStringCache{
-
-        public PrimIntFloat(Format format){
-            super(format);
-        }
+    public static class PrimIntFloat extends FormattedStringCache {
 
         private ArrayList<Float> cachedValues = new ArrayList<>();
         private ArrayList<String> cachedStrings = new ArrayList<>();
+        public PrimIntFloat(Format format) {
+            super(format);
+        }
 
         public String getFormattedValue(float value, int key) {
 
             boolean hasValueAtdataSetIndex = true;
-            if(cachedValues.size() <= key){
+            if (cachedValues.size() <= key) {
                 int p = key;
-                while(p >= 0){
-                    if(p == 0){
+                while (p >= 0) {
+                    if (p == 0) {
                         cachedValues.add(value);
                         cachedStrings.add("");
-                    }else{
+                    } else {
                         cachedValues.add(Float.NaN);
                         cachedStrings.add("");
                     }
@@ -102,12 +101,12 @@ public class FormattedStringCache {
                 hasValueAtdataSetIndex = false;
             }
 
-            if(hasValueAtdataSetIndex) {
+            if (hasValueAtdataSetIndex) {
                 Float cachedValue = cachedValues.get(key);
                 hasValueAtdataSetIndex = !(cachedValue == null || cachedValue != value);
             }
 
-            if(!hasValueAtdataSetIndex){
+            if (!hasValueAtdataSetIndex) {
                 cachedValues.set(key, value);
                 cachedStrings.set(key, mFormat.format(value));
             }
@@ -120,35 +119,22 @@ public class FormattedStringCache {
     /**
      * Cache a Formatted String, derived from formatting float value in the Format passed to the constructor.
      */
-    public static class PrimFloat extends FormattedStringCache{
+    public static class PrimFloat extends FormattedStringCache {
 
-        public PrimFloat(Format format){
+        private HashMap<Float, String> cachedValues = new HashMap<>();
+
+        public PrimFloat(Format format) {
             super(format);
         }
 
-
-        private ArrayList<Float> cachedValues = new ArrayList<>();
-        private ArrayList<String> cachedStrings = new ArrayList<>();
-
         public String getFormattedValue(float value) {
-
-            boolean alreadyHasValue = false;
-            int vCount =  cachedValues.size();
-            int sIndex = -1;
-            for(int i = 0 ; i < vCount ; i++){
-                if(cachedValues.get(i) == value){
-                    sIndex = i;
-                    alreadyHasValue = true;
-                    break;
-                }
+            Float boxedValue = new Float(value);
+            String result = cachedValues.get(boxedValue);
+            if (result == null) {
+                result = mFormat.format(boxedValue);
+                cachedValues.put(boxedValue, result);
             }
-            if(!alreadyHasValue) {
-                cachedValues.add(value);
-                cachedStrings.add(mFormat.format(value));
-                sIndex = cachedValues.size() - 1;
-            }
-
-            return cachedStrings.get(sIndex);
+            return result;
         }
 
     }
@@ -156,39 +142,22 @@ public class FormattedStringCache {
     /**
      * Cache a Formatted String, derived from formatting double value in the Format passed to the constructor.
      */
-    public static class PrimDouble extends FormattedStringCache{
+    public static class PrimDouble extends FormattedStringCache {
 
-        public PrimDouble(Format format){
+        private HashMap<Double, String> cachedValues = new HashMap<>();
+
+        public PrimDouble(Format format) {
             super(format);
         }
 
-
-        private ArrayList<Double> cachedValues = new ArrayList<>();
-        private ArrayList<String> cachedStrings = new ArrayList<>();
-
         public String getFormattedValue(double value) {
-
-            boolean alreadyHasValue = false;
-            int vCount =  cachedValues.size();
-            int sIndex = -1;
-            for(int i = 0 ; i < vCount ; i++){
-                if(cachedValues.get(i) == value){
-                    sIndex = i;
-                    alreadyHasValue = true;
-                    break;
-                }
+            Double boxedValue = new Double(value);
+            String result = cachedValues.get(boxedValue);
+            if (result == null) {
+                result = mFormat.format(boxedValue);
+                cachedValues.put(boxedValue, result);
             }
-            if(!alreadyHasValue) {
-                cachedValues.add(value);
-                cachedStrings.add(mFormat.format(value));
-                sIndex = cachedValues.size() - 1;
-            }
-
-            return cachedStrings.get(sIndex);
+            return result;
         }
-
     }
-
-
-
 }
