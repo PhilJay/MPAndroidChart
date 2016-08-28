443a343c53e5c489bb7d42b67a86b863bb3facad
thekekc <thekekc@mail.ru>
2016-08-28 16:45:19 +0300

    Changed calculateOffsets() method in HorizontalBarChart. Now checks if mCustomViewport is used.

--- MPChartLib/src/main/java/com/github/mikephil/charting/charts/BarLineChartBase.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/charts/BarLineChartBase.java
@@ -931,7 +931,7 @@ public abstract class BarLineChartBase<T extends BarLineScatterCandleBubbleData<
     /**
      * flag that indicates if a custom viewport offset has been set
      */
-    private boolean mCustomViewPortEnabled = false;
+    protected boolean mCustomViewPortEnabled = false;
 
     /**
      * Sets custom offsets for the current ViewPort (the offsets on the sides of
