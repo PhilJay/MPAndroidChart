8e6ba130fad1a4b4beea1122324c2d360e0d4aaa
xuzhenzhou <xuzhenzhou@iyunxiao.com>
2016-09-29 10:45:09 +0800

    Add the setTextColors & getTextColors method for the XAxis Label

--- MPChartLib/src/main/java/com/github/mikephil/charting/renderer/XAxisRenderer.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/renderer/XAxisRenderer.java
@@ -199,8 +199,12 @@ public class XAxisRenderer extends AxisRenderer {
         for (int i = 0; i < positions.length; i += 2) {
 
             float x = positions[i];
+<<<<<<< Updated upstream
             mAxisLabelPaint.setColor(mXAxis.getTextColor((int) (x / 2)));
+=======
+>>>>>>> Stashed changes
             if (mViewPortHandler.isInBoundsX(x)) {
+                mAxisLabelPaint.setColor(mXAxis.getTextColor((int) mXAxis.mEntries[i / 2]));
 
                 String label = mXAxis.getValueFormatter().getFormattedValue(mXAxis.mEntries[i / 2], mXAxis);
 
