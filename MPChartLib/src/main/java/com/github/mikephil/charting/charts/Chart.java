00adf6f184e7bce3f514e16b06731e3b3aa68a7f
jjust <jacob.just@schneider-electric.com>
2017-05-26 12:30:29 +0200

    added null check on set.

--- MPChartLib/src/main/java/com/github/mikephil/charting/charts/Chart.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/charts/Chart.java
@@ -726,6 +726,8 @@ public abstract class Chart<T extends ChartData<? extends IDataSet<? extends Ent
             Highlight highlight = mIndicesToHighlight[i];
 
             IDataSet set = mData.getDataSetByIndex(highlight.getDataSetIndex());
+			if (set == null) 
+				continue;
 
             Entry e = mData.getEntryForHighlight(mIndicesToHighlight[i]);
             int entryIndex = set.getEntryIndex(e);
