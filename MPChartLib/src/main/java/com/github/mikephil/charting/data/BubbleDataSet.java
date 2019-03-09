9d88c927516e3c55a099c92c730a0d6bdaa8d34b
Tom Sager <regas99@yahoo.com>
2019-03-08 17:10:34 -0800

    add BubbleDataSet#getEntriesForXValue and getEntryIndex
    Uses the Bubble Entry containsX() method to find all entries
    containing x.

--- MPChartLib/src/main/java/com/github/mikephil/charting/data/BubbleDataSet.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/data/BubbleDataSet.java
@@ -1,11 +1,14 @@
 
 package com.github.mikephil.charting.data;
 
+import android.util.Log;
+
 import com.github.mikephil.charting.interfaces.datasets.IBubbleDataSet;
 import com.github.mikephil.charting.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class BubbleDataSet extends BarLineScatterCandleBubbleDataSet<BubbleEntry> implements IBubbleDataSet {
 
@@ -68,4 +71,78 @@ public class BubbleDataSet extends BarLineScatterCandleBubbleDataSet<BubbleEntry
     public void setNormalizeSizeEnabled(boolean normalizeSize) {
         mNormalizeSize = normalizeSize;
     }
+
+
+    /**
+     * Returns all Entry objects found at the given x-value,
+     * or an empty array if no Entry object at that x-value.
+     *
+     * We cannot run a binary search here: consider the case
+     * where there is a huge bubble centered at (0, 0)
+     * covering the entire chart - it must be included
+     * in every search result.
+     *
+     * @param xValue
+     * @return closest entries
+     */
+    @Override
+    public List<BubbleEntry> getEntriesForXValue(float xValue) {
+        List<BubbleEntry> entries = new ArrayList<>();
+
+        for (BubbleEntry entry : mValues) {
+            if (entry.containsX(xValue))
+                entries.add(entry);
+        }
+
+        // debug message
+        StringBuilder sb = new StringBuilder(String.format(Locale.getDefault(),
+                "getEntriesForXValue(%.3f) found %d -> ", xValue, entries.size()));
+        for (BubbleEntry e : entries) {
+            sb.append(String.format(Locale.getDefault(), "x= %.1f, y= %.1f, xr= %.1f, yr= %.1f  ", e.getX(), e.getY(), e.getDrawnXRadius(), e.getDrawnYRadius()));
+        }
+        Log.i("BubbleDataSet", sb.toString());
+
+        return entries;
+    }
+
+    /**
+     * Returns the first Entry object whose center is closest to the given xValue and yValue.
+     * Rounding is ignored.
+     *
+     * INFORMATION: This method does calculations at runtime. Do
+     * not over-use in performance critical situations.
+     *
+     * @param xValue the x-value
+     * @param yValue the y-value
+     * @param rounding ignored
+     * @return index of closest entry
+     *
+     *
+     */
+    @Override
+    public int getEntryIndex(float xValue, float yValue, Rounding rounding) {
+        float distance = Float.POSITIVE_INFINITY;
+        int index = -1;
+
+        for (int i = 0; i < mValues.size(); ++i) {
+            BubbleEntry entry = mValues.get(i);
+            float d = distance(entry, xValue, yValue);
+            if (d < distance) {
+                distance = d;
+                index = i;
+            }
+        }
+
+        // debug message
+        Log.i("BubbleDataSet", String.format(Locale.getDefault(), "getEntryIndex(%.2f, %.2f) found %s", xValue, yValue, mValues.get(index)));
+        return index;
+    }
+
+    private float distance(BubbleEntry entry, float x, float y) {
+        if (Float.isNaN(y))
+            return Math.abs(x - entry.getX());
+        else
+            return (float) Math.hypot(entry.getX() - x, entry.getY() - y);
+    }
+
 }
