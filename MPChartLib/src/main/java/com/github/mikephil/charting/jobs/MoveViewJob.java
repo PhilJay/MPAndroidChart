d215e0c04bd39e2f48530c4991089d7db3f217cf
lb.wang <wlb_dlut@163.com>
2019-04-20 23:40:24 +0800

    Fix poolable object leak context issue

--- MPChartLib/src/main/java/com/github/mikephil/charting/jobs/MoveViewJob.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/jobs/MoveViewJob.java
@@ -31,6 +31,12 @@ public class MoveViewJob extends ViewPortJob {
 
     public static void recycleInstance(MoveViewJob instance){
         instance.recycle();
+        // Clear reference avoid memory leak
+        instance.mViewPortHandler = null;
+        instance.xValue = 0f;
+        instance.yValue = 0f;
+        instance.mTrans = null;
+        instance.view = null;
         pool.recycle(instance);
     }
 
