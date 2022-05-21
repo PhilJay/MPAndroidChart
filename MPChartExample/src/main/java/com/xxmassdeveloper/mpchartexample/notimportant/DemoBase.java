a7c27048e42d3f93330ba16eb9a5920a8c7543d8
Mykola Dzyuba <7852483+mdzyuba@users.noreply.github.com>
2022-05-21 14:21:05 -0700

    Gradle plugin update to 7.2
    Updated gradle plugin and dependencies.
    Updated the target SDK.
    Fixed a lint error.
    Fixed javadoc classpath error.

--- MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/notimportant/DemoBase.java
+++ MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/notimportant/DemoBase.java
@@ -60,6 +60,7 @@ public abstract class DemoBase extends AppCompatActivity implements ActivityComp
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSION_STORAGE) {
             if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 saveToGallery();
