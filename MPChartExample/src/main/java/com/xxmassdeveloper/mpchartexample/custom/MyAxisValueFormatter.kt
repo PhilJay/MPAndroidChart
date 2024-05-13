cbcc3d1e87083af2cdfe951d9b40a969948cb67f
Hannes Achleitner <hannes.software@gmx.at>
2024-05-13 08:24:00 +0200

    More Kotlin formatter

---
+++ MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/custom/MyAxisValueFormatter.kt
@@ -0,0 +1,13 @@
+package com.xxmassdeveloper.mpchartexample.custom
+
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.formatter.IAxisValueFormatter
+import java.text.DecimalFormat
+
+class MyAxisValueFormatter : IAxisValueFormatter {
+    private val mFormat = DecimalFormat("###,###,###,##0.0")
+
+    override fun getFormattedValue(value: Float, axis: AxisBase?): String {
+        return mFormat.format(value.toDouble()) + " $"
+    }
+}
