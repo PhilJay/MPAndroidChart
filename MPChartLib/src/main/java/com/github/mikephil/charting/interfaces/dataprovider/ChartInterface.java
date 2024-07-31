00656b3d263c8dc6049414a1f53efa1d28e5f583
Hannes Achleitner <hannes.software@gmx.at>
2024-07-31 07:52:09 +0200

    Remove pointless documentation

--- MPChartLib/src/main/java/com/github/mikephil/charting/interfaces/dataprovider/ChartInterface.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/interfaces/dataprovider/ChartInterface.java
@@ -16,15 +16,11 @@ public interface ChartInterface {
 
     /**
      * Returns the minimum x value of the chart, regardless of zoom or translation.
-     *
-     * @return
      */
     float getXChartMin();
 
     /**
      * Returns the maximum x value of the chart, regardless of zoom or translation.
-     *
-     * @return
      */
     float getXChartMax();
 
@@ -32,22 +28,16 @@ public interface ChartInterface {
 
     /**
      * Returns the minimum y value of the chart, regardless of zoom or translation.
-     *
-     * @return
      */
     float getYChartMin();
 
     /**
      * Returns the maximum y value of the chart, regardless of zoom or translation.
-     *
-     * @return
      */
     float getYChartMax();
 
     /**
      * Returns the maximum distance in scren dp a touch can be away from an entry to cause it to get highlighted.
-     *
-     * @return
      */
     float getMaxHighlightDistance();
 
