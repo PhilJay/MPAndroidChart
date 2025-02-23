06dff01719c8a33ad4792219852514369a745177
Hannes Achleitner <hannes.software@gmx.at>
2025-02-23 08:33:47 +0100

    Rename app

---
+++ app/src/main/java/info/appdev/chartexample/notimportant/ContentItem.kt
@@ -0,0 +1,24 @@
+package info.appdev.chartexample.notimportant
+
+class ContentItem<T : DemoBase> {
+    @JvmField
+    val name: String
+    @JvmField
+    val desc: String
+    @JvmField
+    var isSection = false
+    @JvmField
+    var clazz: Class<T>? = null
+
+    constructor(n: String) {
+        name = n
+        desc = ""
+        isSection = true
+    }
+
+    constructor(n: String, d: String, clazzName: Class<T>) {
+        name = n
+        desc = d
+        clazz = clazzName
+    }
+}
